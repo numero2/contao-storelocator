167b6dbe896aef0418f5c639c78c03257fd7caaa
Michael Boesherz <michael.boesherz@numero2.de>
2021-03-08 15:49:12 +0100

    update comments

--- src/Resources/contao/modules/ModuleStoreLocatorImporter.php
+++ src/Resources/contao/modules/ModuleStoreLocatorImporter.php
@@ -3,13 +3,13 @@
 /**
  * Contao Open Source CMS
  *
- * Copyright (c) 2005-2020 Leo Feyer
+ * Copyright (c) 2005-2021 Leo Feyer
  *
  * @package   StoreLocator
  * @author    Benny Born <benny.born@numero2.de>
  * @author    Michael Bösherz <michael.boesherz@numero2.de>
  * @license   LGPL
- * @copyright 2020 numero2 - Agentur für digitales Marketing GbR
+ * @copyright 2021 numero2 - Agentur für digitales Marketing GbR
  */
 
 
